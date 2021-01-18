c69d36ebff22200d7345a17d4b5e006c8a33825c
Xenofontiy <xenofontiy@gmail.com>
2021-01-18 15:40:59 +0300

    Умножение двух чисел

--- Test_3_sem/Program.cs
+++ Test_3_sem/Program.cs
@@ -10,6 +10,13 @@ namespace Test_3_sem
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ведите первое чило для умножения");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ведите второе чило для умножения");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = x * y;
+            Console.WriteLine ("Умнежение рано "+z);
+            Console.ReadLine();
         }
     }
 }
